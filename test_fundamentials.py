77a584343f52894a9e29bd076059042470dd0997
Tommaso Amorosi <tomy.bawse@gmail.com>
2023-05-09 11:09:14 +0200

    Game of life precommit

--- test_fundamentials.py
+++ test_fundamentials.py
@@ -8,36 +8,38 @@ This will run only tests contain equality_strings in the name.
 
 
 def test_equality_strings():
-    assert 'foobar' == 'foobar'
-
+    assert 'foobar' == 'fobar'
+    # Broken
 
 def test_equality_close_text():
     # Change 1 symbol inside string
-    assert 'Intro to Programming 3: Python' == 'Intro to Programming 3: Python'
-
+    assert 'Intro to Programming 3: Python' == 'Intro to Programming 3:  Python'
+    # Broken
 
 def test_equality_with_long_text():
-    source = '1' * 100 + '2' * 100
+    source = '1' * 100 * '2' * 100 #100 * '2' broke this test.
     with_a = source  # .replace('12', 'a')
     with_b = source  # .replace('12', 'b')
     assert with_a == with_b
-
+    # Broken
 
 def test_equality_list():
-    assert [0, 1, 3] == [0, 1, 3]
-
+    assert [0, 1, 3, 420] == [0, 1, 3, 69]
+    # Broken
 
 def test_equality_dict():
-    assert {'banana': 1, 'cherry': 4} == {'banana': 1, 'cherry': 4}
+    assert {'banana': 1, 'cherry': 4, 'Kiwi': 1} == {'banana': 1, 'cherry': 4, 'apple': 13}
+    # Broken
 
 
 def test_equality_set():
-    assert {'banana', 'cherry'} == {'banana', 'cherry', 'banana'}
-
+    assert {'banana', 'cherry', 'orange'} == {'banana', 'cherry', 'banana'}
+    # Broken
 
 def test_equality_list_different_length():
-    assert list('1234') == ['1', '2', '3', '4']
-
+    assert list('12345') == ['1', '2', '3', '4']
+    # Broken
 
 def test_in():
-    assert 'Space' in 'Harbour.Space'
+    assert 'Space' in 'Harbour.space'
+    # Broken
